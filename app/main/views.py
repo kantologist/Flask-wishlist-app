c6ef32c3dbf2391533c06c06789e19f9dc0e8300
femi <azeezfemi17937@yahoo.com>
2016-12-20 12:34:42 +0100

    initial commit

---
+++ app/main/views.py
@@ -0,0 +1,199 @@
+from flask import Flask, request, make_response, redirect, url_for, \
+                    render_template, session, flash, current_app
+from . import main
+from .forms import EditProfileForm, EditProfileAdminForm, WishForm
+from .. import db
+from ..models import User, Permission, Role, Product, WishList, Item
+from ..email import send_mail
+from datetime import datetime
+from flask import abort
+from flask_login import login_required, current_user
+from ..decorators import admin_required, permission_required
+
+
+@main.route('/')
+def index():
+    page = request.args.get('page', 1, type=int)
+    pagination = Product.query.order_by(Product.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POST_PER_PAGE'],
+        error_out=False)
+    products = pagination.items
+    if current_user.is_authenticated:
+        wishlists = current_user.wishlists.all()
+        if wishlists == []:
+            wishlist = WishList(name='default')
+            current_user.wishlists.append(wishlist)
+            db.session.add(current_user)
+            db.session.commit()
+        wishlists = current_user.wishlists.all()
+        wished=[]
+        for wishlist in wishlists:
+            for item in wishlist.items.all():
+                 wished.append(item.item)
+    else:
+        wished=[]
+        wishlists=[]
+    return render_template('index.html',
+                           products = products,
+                           pagination=pagination,
+                           wished=wished,
+                           wishlists=wishlists)
+
+@main.route('/<product_id>/<wishlist>/add-to-wishlist')
+def add_to_wishlist(product_id, wishlist):
+    product = Product.query.get_or_404(product_id)
+    item = Item(product_id=product.id)
+    wishlist = WishList.query.filter_by(owner_id=current_user.id,
+                                         name=wishlist).first()
+    wishlist.items.append(item)
+    db.session.add(wishlist)
+    db.session.commit()
+    return redirect(url_for('.index'))
+
+@main.route('/add-wishlist', methods=['GET', 'POST'])
+@login_required
+def add_wishlist():
+    form = WishForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        wishlist = WishList(name=name,
+                            owner_id=current_user.id)
+        db.session.add(wishlist)
+        db.session.commit()
+        return redirect(url_for('.index'))
+    return render_template('new_wishlist.html', form=form)
+
+@main.route('/<wishlist>/remove-wishlist')
+def remove_wishlist(wishlist):
+    wishlist = WishList.query.filter_by(owner_id=current_user.id,
+                                         name=wishlist).first()
+    db.session.delete(wishlist)
+    db.session.commit()
+    return redirect(url_for('.index'))
+
+
+
+@main.route('/<username>/<wishlist>')
+def wishlist(username, wishlist):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    all_wishlists= user.wishlists.all()
+    wishlists = WishList.query.filter_by(owner_id=current_user.id,
+                                         name=wishlist).first()
+
+    wished=[]
+    for item in wishlists.items.all():
+             wished.append(item.item)
+    items = wishlists.items.all()
+    products=[]
+    for item in items:
+        products.append(item.item)
+    return render_template('wishlist.html',
+                           user=user,
+                           products=products,
+                           wishlists=all_wishlists,
+                           wished=wished)
+
+@main.route('/<product_id>/remove-from-wishlist')
+def remove_from_wishlist(product_id):
+    product = Product.query.get_or_404(product_id)
+    item = product.items.first()
+    wishlist=item.wish
+    if wishlist.name != 'default':
+        wishlist.items.remove(item)
+        db.session.add(wishlist)
+        db.session.commit()
+    return redirect(url_for('.index'))
+
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    if current_user.is_authenticated:
+        wishlists = current_user.wishlists.all()
+        if wishlists == []:
+            wishlist = WishList(name='default')
+            current_user.wishlists.append(wishlist)
+            db.session.add(current_user)
+            db.session.commit()
+        wishlists = current_user.wishlists.all()
+        wished=[]
+        for wishlist in wishlists:
+            for item in wishlist.items.all():
+                 wished.append(item.item)
+    else:
+        wishlists=[]
+    return render_template('user.html', user=user,
+                           wishlists=wishlists,
+                           wished=wished)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
+
+
+@main.route('/product/<int:id>')
+def product(id):
+    product = Product.query.get_or_404(id)
+    if current_user.is_authenticated:
+        wishlists = current_user.wishlists.all()
+        if wishlists == []:
+            wishlist = WishList(name='default')
+            current_user.wishlists.append(wishlist)
+            db.session.add(current_user)
+            db.session.commit()
+        wishlists = current_user.wishlists.all()
+        wished=[]
+        for wishlist in wishlists:
+            for item in wishlist.items.all():
+                 wished.append(item.item)
+    else:
+        wishlists=[]
+    return render_template('product.html', products=[product],
+                           wishlists=wishlists,
+                           wished=wished)
---
+++ app/models.py
@@ -0,0 +1,258 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, AnonymousUserMixin
+from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app, request
+from datetime import datetime
+import hashlib
+from markdown import markdown
+import bleach
+from sqlite3 import IntegrityError
+
+class Permission():
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT , True),
+            'Moderator':(Permission.COMMENT|
+                         Permission.MODERATE_COMMENTS, False),
+            'Administrator':(0xff, False)
+        }
+
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+class WishList(db.Model):
+    __tablename__ = 'wishlists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    items = db.relationship('Item',
+                               backref='wish',
+                               lazy = 'dynamic',
+                               cascade='all, delete-orphan')
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('wishlists.id'))
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
+    wishlists = db.relationship('WishList',
+                               backref='owner',
+                               lazy = 'dynamic',
+                               cascade='all, delete-orphan'
+                               )
+
+    def __init__(self, **kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None and self.avatar_hash is None:
+            self.avatar_hash =  hashlib.md5(
+                self.email.encode('utf-8')).hexdigest()
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
+    def can(self, permissions):
+        return self.role is not None and\
+            (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable  attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+    def gravatar(self, size=100, default = 'identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or\
+         hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size,
+            default=default, rating=rating)
+
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+            password=forgery_py.lorem_ipsum.word(),
+                     username=forgery_py.internet.user_name(True),
+                     confirmed=True,
+                     name=forgery_py.name.full_name(),
+                     location =forgery_py.address.city(),
+                     about_me=forgery_py.lorem_ipsum.sentence(),
+                     member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=datetime.utcnow)
+    items = db.relationship('Item',
+                               backref='item',
+                               lazy = 'dynamic',
+                               cascade='all, delete-orphan')
+
+
+    def gravatar(self):
+        return self.image_url
+
+    def __repr__(self):
+        return '<Product %r>' % self.name
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint, choice
+        import forgery_py
+
+        shirts=['https://encrypted-tbn0.gstatic.com/images?'
+                'q=tbn:ANd9GcSu_vmUgVc4hY911e2q5w7YxC1oujBho'
+                'gJdgLCIpUrwBMglSgUl&reload=on']
+        shirts.append('https://encrypted-tbn0.gstatic.com/'
+                      'images?q=tbn:ANd9GcQeS-GnHV9g7jN6r'
+                      'mfMwHHAMJWiotIXbUnEjQhxAuIPvpV9LldNJg')
+        shirts.append('https://encrypted-tbn3.gstatic.com/images'
+                      '?q=tbn:ANd9GcTP_z5W2I--ROhM8UgfjRMc4bDePv9T'
+                      'n_YKLjgxLBaOt97h5XUqFg')
+        shirts.append('https://encrypted-tbn1.gstatic.com/image'
+                      's?q=tbn:ANd9GcQxO0wv7UrRqaOCC8xvO9wHiDzI1'
+                      '-9pX2e9zmOs1MSl2LDCCu2Ksg')
+        shirts.append('https://encrypted-tbn2.gstatic.com/images'
+                      '?q=tbn:ANd9GcT9hIUiWzOC_677rOL7y4oXz3XJ9r'
+                      '1fsJxxzMlYWGLXtKc7gnpp3A')
+        shirts.append('https://encrypted-tbn2.gstatic.com/images'
+                      '?q=tbn:ANd9GcQxj3fggfrlYNBtaGS0sAONLB2ADZf'
+                      'cAmh3aiEnTPFChiqg_J8L')
+        shirts.append('https://encrypted-tbn2.gstatic.com/'
+                      'images?q=tbn:ANd9GcRtwI1hhG1M-pKc_'
+                      '0XyBNDbMyi6eBhprpodSfSyXpB-Fa4HisAK2Q')
+        shirts.append('https://encrypted-tbn0.gstatic.com/images'
+                      '?q=tbn:ANd9GcRPf0hPH7yP6o-9QsyThXYDGpN9b6i'
+                      '_ISTKhlXfIk5DLTtW1q7uNQ')
+        shirts.append('https://encrypted-tbn3.gstatic.com/images?q=tbn'
+                      ':ANd9GcT6RdupeyZsthKw4ZiYM48ev_35pQocsU6XQ'
+                      'B_W5qJqTckPddwL&reload=on')
+        shirts.append('https://encrypted-tbn0.gstatic.com/images?'
+                      'q=tbn:ANd9GcTRtHCML-AjEaEe2gPTdDbdCZ2by1l'
+                      '4SOejeVWD4Bt-va9sy0Xp')
+
+        seed()
+        for i in range(count):
+            p = Product(name= forgery_py.name.industry(),
+                        description=\
+                        forgery_py.lorem_ipsum.sentence(),
+                        image_url=choice(shirts),
+                     timestamp=forgery_py.date.date(True))
+            db.session.add(p)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
+
+
+class AnonymousUser(AnonymousUserMixin):
+
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+login_manager.anonymous_user = AnonymousUser
